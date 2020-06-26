8084c2ba4a1c6af4e6296c44144717c0d660abd3
Yusuke Kato <kpango@vdaas.org>
2020-06-26 11:17:01 +0900

    small refactor (#1)
    small refactor

--- loader/loader.go
+++ loader/loader.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"sync/atomic"
 	"time"
 
 	"github.com/kpango/glg"
@@ -19,30 +18,24 @@ type Object struct {
 	Class  string
 }
 
-func New(s string) ([]*Object, error) {
-	var data []*Object
-	var err error = nil
+func New(path string) (datas []*Object, err error) {
+	f, err := os.Open(path)
 
-	f, e := os.Open(s)
+	if err != nil {
+		glg.Error(err)
+		return nil, err
+	}
 	defer f.Close()
 
-	if e != nil {
-		glg.Error(e)
-		panic(e)
-	}
-
-	var id int32 = 0
-
 	line := bufio.NewScanner(f)
-	for line.Scan() {
+	for i := int32(0); line.Scan(); i++ {
 		str := strings.Split(line.Text(), ",")
 		// minimun len(str) is 1
 		if len(str) <= 1 {
 			break
 		}
-
-		var tmp = Object{
-			ID:    id,
+		var data = Object{
+			ID:    i,
 			Class: str[len(str)-1],
 		}
 		str = str[:len(str)-1]
@@ -50,35 +43,29 @@ func New(s string) ([]*Object, error) {
 			f64, err := strconv.ParseFloat(v, 64)
 			if err != nil {
 				glg.Error(err)
-				panic(err)
+				return nil, err
 			}
-			tmp.Vector = append(tmp.Vector, f64)
+			data.Vector = append(data.Vector, f64)
 		}
-		data = append(data, &tmp)
-		var oldId = id
-		id = atomic.AddInt32(&oldId, 1)
+		datas = append(datas, &data)
 	}
 	if err = line.Err(); err != nil {
 		glg.Error(err)
-		panic(err)
+		return nil, err
 	}
-	return data, err
+	return datas, err
 }
 
 func Set(data []*Object, r float64) ([]*Object, []*Object) {
 	// generate id list for test
 	rand.Seed(time.Now().UnixNano())
-	var testIds []int32
-	var c int32 = 0
-	for c < int32(float64(len(data))*r) {
-		n := rand.Int31n(int32(len(data)))
-		testIds = append(testIds, n)
-		var tmp = c
-		c = atomic.AddInt32(&tmp, 1)
+	testIds := make([]int32, 0, len(data))
+	for c := int32(0); c < int32(float64(len(data))*r); c++ {
+		testIds = append(testIds, rand.Int31n(int32(len(data))))
 	}
 	// devide data into Train and Test
-	var train []*Object
-	var test []*Object
+	train := make([]*Object, 0, len(data))
+	test := make([]*Object, 0, len(data))
 	for _, d := range data {
 		if util.IntContains(testIds, d.ID) {
 			test = append(test, d)
